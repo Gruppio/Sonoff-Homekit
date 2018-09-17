bb825544505cfb40f2aef38112f01d4160359502
Michele Gruppioni <gruppiofigo@hotmail.it>
2018-09-17 22:17:29 +0200

    minor changes

--- flash.sh
+++ flash.sh
@@ -5,9 +5,9 @@ esptool.py \
             -p $SONOFF_PORT \
             --baud 115200 \
             write_flash \
-            -fs 8 \
+            -fs 8m \
             -fm dout \
             -ff 40m \
-            0x0 ./common/rboot.bin \
-            0x1000 ./common/blank_config.bin \
-            0x2000 ./homekit_projects/sonoff.bin
+            0x0 ./firmware/rboot.bin \
+            0x1000 ./firmware/blank_config.bin \
+            0x2000 ./firmware/sonoff.bin
